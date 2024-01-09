4e1687735c6c931e835d5453f080121c0469eddb
DineshPhade <dinesh.phade07@gmail.com>
2024-01-09 16:41:12 +0530

    success

--- src/test/java/InvoiceTest.java
+++ src/test/java/InvoiceTest.java
@@ -1,3 +1,20 @@
-public class InvoiceTest {
+import org.example.Invoice;
+import org.junit.Test;
 
+public class InvoiceTest
+{
+	Invoice i=new Invoice();
+	
+	@Test
+	public void Testfair()
+	{
+		double distance=2;
+		int time=5;
+		double result=i.calcualtorFare(2, 5);
+	}
+	@Test
+	public void TestRide()
+	{
+	double result1=i.InvoiceSummary(12, 4);	
+	}
 }
