17cb33f9923a85535066bbf863acd5852111e65a
Jyoshna-gaykwad <tilharisuhana@gmail.com>
2024-01-09 13:29:12 +0530

    updated code

--- src/main/java/org/example/Invoice.java
+++ src/main/java/org/example/Invoice.java
@@ -5,7 +5,7 @@ public class Invoice {
     static final double costpermin=1.0;
     static final double min_fare=5;
     public double CalcualtorFare(double distance,int time){
-        double totalfare=(distance*costperkm)*(time*costperkm);
+        double totalfare=(distance*costperkm)+(time*costperkm);
         if(totalfare<5){
             return min_fare;
         }
